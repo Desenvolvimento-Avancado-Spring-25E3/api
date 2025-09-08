300169d44edbf7419d7f19b0435152a2025caf58
Elberth Lins Costa de Moraes <elberth.moraes@dataprev.gov.br>
2025-09-08 16:33:20 -0300

    TDD

---
+++ elberthpedidoapi/src/main/java/br/edu/infnet/elberthpedidoapi/ElberthpedidoapiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.elberthpedidoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElberthpedidoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElberthpedidoapiApplication.class, args);
+	}
+
+}
