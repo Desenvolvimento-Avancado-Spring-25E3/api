abad12e7440fe3102fbf7f2c04d4b2b3c1f48666
Elberth Lins Costa de Moraes <elberth.moraes@dataprev.gov.br>
2025-09-20 10:36:30 -0300

    Criacao do projeto multi-modulos: common-domain e external-api finalizados.

---
+++ elberth-parent/comman-domain/src/main/java/br/edu/infnet/mono/CommanDomainApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.mono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommanDomainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommanDomainApplication.class, args);
+	}
+
+}
---
+++ elberth-parent/comman-domain/src/main/java/br/edu/infnet/mono/model/domain/Vendedor.java
@@ -0,0 +1,126 @@
+package br.edu.infnet.mono.model.domain;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+
+@Entity
+@Table(name = "vendedores")
+public class Vendedor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(unique = true, nullable = false, length = 14)
+    private String cpf;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false, length = 15)
+    private String telefone;
+
+    @Column(unique = true, nullable = false)
+    private Integer matricula;
+
+    @Column(nullable = false)
+    private Double salario;
+
+    private boolean ativo = true;
+
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    private Endereco endereco;
+
+    @Transient
+    private String cepInput;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public Integer getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(Integer matricula) {
+		this.matricula = matricula;
+	}
+
+	public Double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(Double salario) {
+		this.salario = salario;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getCepInput() {
+		return cepInput;
+	}
+
+	public void setCepInput(String cepInput) {
+		this.cepInput = cepInput;
+	}
+}
